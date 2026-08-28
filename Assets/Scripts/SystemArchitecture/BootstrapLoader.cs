5e4aa4ba9e13f9e3559c0dd585d9344544524f11
CDelgado0x0 <cdelgado@uji.es>
2026-08-28 14:03:25 +0200

    CambiosPeques

--- Assets/Scripts/SystemArchitecture/BootstrapLoader.cs
+++ Assets/Scripts/SystemArchitecture/BootstrapLoader.cs
@@ -4,18 +4,14 @@ using UnityEngine.SceneManagement;
 public class BootstrapLoader : MonoBehaviour
 {
     [SerializeField] private GameObject settingsManagerPrefab;
-    [SerializeField] private GameObject audioManagerPrefab;
     [SerializeField] private GameObject brightnessOverlayPrefab;
     [SerializeField] private GameObject metricsManagerPrefab;
-    [SerializeField] private GameObject gameManagerPrefab;
 
     private void Awake()
     {
         Instantiate(brightnessOverlayPrefab);
         Instantiate(settingsManagerPrefab);
-        Instantiate(audioManagerPrefab);
         Instantiate(metricsManagerPrefab);
-        Instantiate(gameManagerPrefab);
     }
 
     private void Start()
